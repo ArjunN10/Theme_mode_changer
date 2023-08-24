f1cb9610bb6f06dc73d4387987a8c0e8d3bebbf9
ARJUN N <138310139+ArjunN10@users.noreply.github.com>
2023-08-24 22:58:35 +0530

    first commit

--- src/mode/DarkWhiteMode.js
+++ src/mode/DarkWhiteMode.js
@@ -5,10 +5,11 @@ const [darkmode,setconvert]=useState(false)
 const change=()=>{
     setconvert(!darkmode)
 }
-
+   
   return (
     <div style={{background:darkmode?'black':'white'}} className='dark'>
-    <div style={{marginBottom:110}}>    <h3 style={{color:darkmode?'white':'black'}}>Click the Button for Mode change </h3>
+    <div style={{marginBottom:110}}>  
+      <h3 style={{color:darkmode?'white':'black'}}>Click the Button for Mode change </h3>
     <button onClick={change} className='btn'>click to change</button>
     </div>
 
