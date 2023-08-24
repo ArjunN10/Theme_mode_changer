2c1740dcfa0ae875087603c42dd3a122a7efb93e
ARJUN N <138310139+ArjunN10@users.noreply.github.com>
2023-08-24 11:04:20 +0530

    first commit

---
+++ src/mode/DarkWhiteMode.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import './Dark.css'
+function DarkWhiteMode() {
+const [darkmode,setconvert]=useState(false)
+const change=()=>{
+    setconvert(!darkmode)
+}
+
+  return (
+    <div style={{background:darkmode?'black':'white'}} className='dark'>
+    <div style={{marginBottom:110}}>    <h3 style={{color:darkmode?'white':'black'}}>Click the Button for Mode change</h3>
+    <button onClick={change} className='btn'>click to change</button>
+    </div>
+
+    </div>
+  )
+}
+
+export default DarkWhiteMode
